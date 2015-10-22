bb4d6e1d065fb49daecfd818c985b5e545b89fa8
wanglichun <wanglichun@wanglichundeMac-mini.local>
2015-10-22 10:43:40 +0800

    调整文件组织结构；添加游戏主面板

---
+++ SnakeGame/Food.swift
@@ -0,0 +1,13 @@
+//
+//  Food.swift
+//  SnakeGame
+//
+//  Created by wanglichun on 15/10/22.
+//  Copyright © 2015年 thunder. All rights reserved.
+//
+
+import UIKit
+
+class Food: NSObject {
+
+}
